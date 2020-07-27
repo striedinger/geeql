1e25c0d7c01f81d66af94b7cd2ccea1e88fb5eeb
Hugo Striedinger <hugo.striedinger@dowjones.com>
2020-07-27 19:16:52 -0400

    another serverless attempt

---
+++ serverless/playground.js
@@ -0,0 +1,10 @@
+const { GraphQLServerLambda } = require('graphql-yoga');
+const typeDefs = require('../src/types');
+const resolvers = require('../src/resolvers');
+
+const lambda = new GraphQLServerLambda({ 
+  typeDefs,
+  resolvers
+});
+
+module.exports = lambda.playgroundHandler;
